3ce8eb223288aecc398ec176a23d286d01d9bae1
Karl2242 <kchikhicloud@gmail.com>
2024-11-28 22:16:17 +0100

    ADD: button for add student

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,7 @@ module.exports = {
         "box-colors": "rgb(121,117,131)",
         "box-gradient":
           "linear-gradient(71deg, rgba(121,117,131,0.4654236694677871) 0%, rgba(54,53,103,0.32816876750700286) 100%)",
+          "color-button": "rgba(21,21,21)"
       },
     },
   },
