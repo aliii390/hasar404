e26b3a844c2dfe49307969a798ee1b6079dadb56
Karl2242 <kchikhicloud@gmail.com>
2024-11-29 16:15:30 +0100

    Liste

--- script.js
+++ script.js
@@ -8,28 +8,5 @@ console.log(tableau);
 btnEntrez.addEventListener("click", handleSubmit);
 
 
-function handleSubmit(tableau){
-  let nameStudent = inputNom.value;
-pushtableau(nameStudent)
-
-return nameStudent;
-}
-
-
-function pushtableau(nameStudent, listeNom){
-tableau.push(nameStudent)
-console.log(tableau)
-
-for(let i = 0; i < tableau.length; i = i + 1){
-let addName = document.createElement("li")
-addName.innerHTML= tableau[i]
-console.log(addName)
-
-document.querySelector("ol").appendChild(addName)
-
-console.log(listeNom)
-}
-
-}
 
 
