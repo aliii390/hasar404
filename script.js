958481d06d79d60382c035acde1f44f2b3cff1a6
aliii390 <alli.said.errahmani@gmail.com>
2024-11-29 16:15:44 +0100

    ADD: fin de code pour afficher les listes (ALI)

--- script.js
+++ script.js
@@ -1,24 +1,50 @@
 const inputNom = document.getElementById("studentInput");
 const btnEntrez = document.getElementById("enter");
 const listeNom = document.getElementById("listOl");
-let tableau; 
+// const listeLi = document.getElementById("listLi")
+
+
+
+
+// code ALI
+
+let tableau = [];
 
 console.log(listeNom);
 
-
 btnEntrez.addEventListener("click", handleSubmit);
 
+function handleSubmit() {
+  //
+  // let testing = listeLi.innerText  += textt;
+  // console.log(listeNom);
+
+  // newList.textContent = textt;
+
+  // listeNom.appendChild(newList)
+
+  // for(let tab of tableau){
+  //     let textt = inputNom.value;
+  //     let newList = document.createElement('li');
+  //     newList.innerText = tab
+  // }
+
+  let textt = inputNom.value;
+
+  tableau.push(textt);
 
 
+  let newLi = document.createElement("li");
+  newLi.innerText = textt;
 
-function handleSubmit(){
-    let textt = inputNom.value;
-     console.log(textt);
-    textt.innerHtml = "<li> test </li>"
+   listeNom.appendChild(newLi);
+  inputNom.value = '';
 
+  
+
+  console.log(tableau);
+  
 }
 
 
-
-
-
+// FIN DE CODE ALI
