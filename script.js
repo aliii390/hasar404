3130576b5887bf2203a1c91d47735552449e61fc
Karl2242 <kchikhicloud@gmail.com>
2024-11-29 15:11:06 +0100

    ADD: interactie list

--- script.js
+++ script.js
@@ -1,24 +1,35 @@
 const inputNom = document.getElementById("studentInput");
 const btnEntrez = document.getElementById("enter");
-const listeNom = document.getElementById("listOl");
-let tableau; 
-
-console.log(listeNom);
+let tableau = [];
+let count = 0;
+console.log(tableau);
 
 
 btnEntrez.addEventListener("click", handleSubmit);
 
 
+function handleSubmit(tableau){
+  let nameStudent = inputNom.value;
+pushtableau(nameStudent)
 
-
-function handleSubmit(){
-    let textt = inputNom.value;
-     console.log(textt);
-    textt.innerHtml = "<li> test </li>"
-
+return nameStudent;
 }
 
 
+function pushtableau(nameStudent, listeNom){
+tableau.push(nameStudent)
+console.log(tableau)
 
+for(let i = 0; i < tableau.length; i = i + 1){
+let addName = document.createElement("li")
+addName.innerHTML= tableau[i]
+console.log(addName)
+
+document.querySelector("ol").appendChild(addName)
+
+console.log(listeNom)
+}
+
+}
 
 
