71676348b66f909197dc66c80159a205d89de448
Karl2242 <kchikhicloud@gmail.com>
2024-12-01 16:38:45 +0100

    Clean Merge

--- script.js
+++ script.js
@@ -4,11 +4,17 @@ const listeNom = document.getElementById("list");
 // const listeLi = document.getElementById("listLi")
 // code ALI
 let tableau = [];
+const buttonForGenerate = document.getElementById("buttonGenerer")
+let mesPlace = document.querySelectorAll("#placeStudent > div > article > p")
+let count = 0;
+
 
 console.log(listeNom);
 
 
 btnEntrez.addEventListener("click", handleSubmit);
+buttonForGenerate.addEventListener("click", handleOnClickAleatoirePlacement)
+
 
 function handleSubmit() {
 
@@ -30,5 +36,28 @@ function handleSubmit() {
   
 }
 // FIN DE CODE ALI
+
+
+
+
+
+function handleOnClickAleatoirePlacement(){   
+  console.log("test");
+  
+    const listeRandomiser = [...tableau];
+    for(let i = listeRandomiser.length - 1; i > 0; i--){
+        let j = Math.floor(Math.random() * (i - 1));
+        [listeRandomiser[i], listeRandomiser[j]] = [listeRandomiser[j], listeRandomiser[i]]   
+    }
+    
+    placement(listeRandomiser);
+
+}
+function placement(listeRandomiser){
+    mesPlace.forEach((place, index) => {
+        place.innerHTML = listeRandomiser[index];
+        
+    })
+}
 
 
