a74433081342fa0ecca8d2b4604cd52d24ac9bde
Karl2242 <kchikhicloud@gmail.com>
2024-12-02 12:31:21 +0100

    start drag & drop

--- script.js
+++ script.js
@@ -7,10 +7,13 @@ let tableau = [];
 const buttonForGenerate = document.getElementById("buttonGenerer")
 let mesPlace = document.querySelectorAll("#placeStudent > div > article > p")
 let count = 0;
-
+let addDiv = document.getElementById("addDiv")
+let divPourAdd = document.getElementById("placeStudent")
+let divTarget;
 
 btnEntrez.addEventListener("click", handleSubmit);
 buttonForGenerate.addEventListener("click", handleOnClickAleatoirePlacement)
+addDiv.addEventListener("click", handleClickAddDiv)
 
 
 function handleSubmit() {
@@ -57,4 +60,36 @@ function placement(listeRandomiser){
     })
 }
 
+function handleClickAddDiv(){
 
+  let newDiv = document.createElement("article")
+newDiv.classList.add("w-[100px]", "justify-center", "items-center", "flex", "shadow-xl", "h-[100px]", "border-opacity-[20%]", "border-white", "border-[0.1px]", "rounded-[14%]", "bg-gradient-to-r", "from-box-premiere-couleur", "to-box-deuxieme-couleur", "opacity-[1]", "blur-[0.5px]")  
+divPourAdd.appendChild(newDiv)
+
+newDiv.addEventListener("mousedown", handlePressMoveDiv)
+
+return newDiv
+}
+
+
+function handlePressMoveDiv(newDiv){
+divTarget = newDiv.target
+
+  divTarget.classList.add("absolute", "z-[1000]")
+  
+
+  divTarget.addEventListener("mousemove", handleMouseMoveFollow)
+
+return divTarget;
+}
+
+
+function moveAt(pageX, pageY){
+  divTarget.style.left = pageX - divTarget.offsetWidth / 2 + "px";
+  divTarget.style.top = pageY - divTarget.offsetWidth / 2 + "px";
+}
+
+
+function handleMouseMoveFollow(event){ 
+moveAt(event.pageX, event.pageY);
+}
